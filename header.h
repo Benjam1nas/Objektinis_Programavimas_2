fc3afe9b816a568b9e5af5848bcc8f10deea440d
Benjam1nas <116347949+Benjam1nas@users.noreply.github.com>
2023-03-17 08:43:28 +0200

    Update header.h

--- header.h
+++ header.h
@@ -20,7 +20,9 @@ bool lyginimas_3(const studentas& a, const studentas& b);
 bool lyginimas_4(const studentas& a, const studentas& b);
 void atsitiktinis(vector<studentas>& temp, int m);
 void pild(vector<studentas>& temp, int& m);
-void pild_failas(vector <studentas>& temp, int& m);
-void spausd(vector<studentas> temp, string skaiciuokle);
+void pild_failas(vector <studentas>& temp, int& m, string pav);
+void spausd(vector<studentas> temp, string skaiciuokle, string out_pav);
+void generavimas(string& pav, double &generavimo_laikas);
+void skirstymas(vector<studentas>& temp, vector<studentas>& liudnukai, vector<studentas>& linksmukai);
 
 #endif
